24caa2d050443f843812ed8995c6dc9c7576817a
Seyda0156 <seydanurovaci@gmail.com>
2024-12-28 08:04:34 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Migrations/202412280040034_mig1.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpEgitimKampi301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "CategoryStatus", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.Products", "ProductStock", c => c.Int(nullable: false));
+            AlterColumn("dbo.Products", "ProductPrice", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "ProductPrice", c => c.String());
+            AlterColumn("dbo.Products", "ProductStock", c => c.String());
+            AlterColumn("dbo.Categories", "CategoryStatus", c => c.String());
+        }
+    }
+}
